7a30a2cc6d5624c41e6957812200e8981217f484
Gabriel Benedito <gabrielh.beneditoo@gmail.com>
2025-10-24 11:14:16 -0300

    Conexão com DB ✔

--- back-end/conexao.py
+++ back-end/conexao.py
@@ -1,4 +1,27 @@
 # pip install fastapi uvicorn
 # pip install mysql-connector-python
-# pip dotenv
+# pip install dotenv
+import mysql.connector
+from dotenv import load_dotenv
+import os
 
+#Carregar .env
+load_dotenv()
+
+def connector():
+    try:
+        conexao = mysql.connector.connect(
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
+        )
+        cursor = conexao.cursor()
+        print("Conexão estabelecida!")
+        return conexao, cursor
+    
+    except Exception as erro:
+        print(f"ERRO de conexão: {erro} ")
+        return None, None
+connector()
